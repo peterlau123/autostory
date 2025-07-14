2143c637ef25e0737858a12659c1fb0728d60db5
liuxin <liuxin@sensetime.com>
2025-07-14 22:32:00 +0800

    feat: add requirements and deepseek api

--- autostory/deepseek.py
+++ autostory/deepseek.py
@@ -1,6 +1,7 @@
 import os
 
 
-
-
-api_key=os.environ['DEEP_SEEK_API_KEY'] # DEEP_SEEK_API_KEY=sk-e9d9c2f2f9a04b3fa2f25188e81e62e1
+class DeepSeekApi:
+    def get_key(self):
+        api_key=os.environ['DEEP_SEEK_API_KEY'] # DEEP_SEEK_API_KEY=sk-e9d9c2f2f9a04b3fa2f25188e81e62e1
+        return api_key
